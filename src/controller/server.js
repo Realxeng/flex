6b04c2595f06a5591464d600f5824274d86dd0eb
Realxeng <realxengpandelaki@gmail.com>
2025-08-12 16:43:09 +0800

    Refactor server and interaction handling: migrate to new structure, implement scenery handling, and improve command processing

--- src/controller/server.js
+++ src/controller/server.js
@@ -3,20 +3,15 @@
  */
 
 import { AutoRouter } from 'itty-router';
-import {
-  InteractionResponseType,
-  InteractionType,
-  MessageComponentTypes,
-  ButtonStyleTypes,
-  verifyKey,
-} from 'discord-interactions';
-import { TEST_COMMAND, CHECK_SCENERY_COMMAND, CHECK_ONLINE_ATC, MONITOR_VATSIM, REMOVE_NOTIF } from './tool/commands.js';
-import { getSceneryVersion, checkReleased, sendSceneryFile, sendOnlineATC, addReminder, removeNotification } from './model/logic.js'
-import { DiscordRequest } from './controller/discordController.js';
-import { sendReminderAdd, sendReminderMin } from './view/discordMessages.js';
-import { getOnlineATC } from './model/API/vatsimAPI.js';
+import { InteractionResponseType, InteractionType } from 'discord-interactions';
+import { TEST_COMMAND, CHECK_SCENERY_COMMAND, CHECK_ONLINE_ATC, MONITOR_VATSIM, REMOVE_NOTIF } from '../tool/commands.js';
+import { sendSceneryFile, sendOnlineATC, addReminder, removeNotification } from '../model/logic.js'
+import { sendReminderAdd, sendReminderMin } from '../view/discordMessages.js';
+import { getOnlineATC } from '../model/API/vatsimAPI.js';
+import { sceneryHandler } from '../model/sceneryModel.js';
+import { verifyDiscordRequest } from '../model/discordVerify.js';
 
-class JsonResponse extends Response {
+export class JsonResponse extends Response {
   constructor(body, init) {
     const jsonBody = JSON.stringify(body);
     init = init || {
@@ -30,37 +25,32 @@ class JsonResponse extends Response {
 
 const router = AutoRouter();
 
-/**
- * A simple :wave: hello page to verify the worker is working.
- */
+//Simple hello page
 router.get('/', (request, env) => {
   return new Response(`👋 ${env.DISCORD_APPLICATION_ID}`);
 });
 
-/**
- * Main route for all requests sent from Discord.  All incoming messages will
- * include a JSON payload described here:
- * https://discord.com/developers/docs/interactions/receiving-and-responding#interaction-object
- */
+//Interaction Router
 router.post('/', async (request, env, ctx) => {
-  const { isValid, interaction } = await verifyDiscordRequest(
-    request,
-    env,
-  );
+  //Verify request
+  const { isValid, interaction } = await verifyDiscordRequest(request, env);
+  //Check verification
   if (!isValid || !interaction) {
     return new Response('Bad request signature.', { status: 401 });
   }
 
+  //Respond to ping request
   if (interaction.type === InteractionType.PING) {
-    // The `PING` message is used during the initial webhook handshake, and is
-    // required to configure the webhook in the developer portal.
     return new JsonResponse({
       type: InteractionResponseType.PONG,
     });
   }
 
+  //Handle command interactions
   if (interaction.type === InteractionType.APPLICATION_COMMAND) {
+    //Check the command type
     switch (interaction.data.name.toLowerCase()) {
+      //test command
       case TEST_COMMAND.name.toLowerCase(): {
         return new JsonResponse({
           type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
@@ -69,76 +59,18 @@ router.post('/', async (request, env, ctx) => {
           },
         });
       }
+      //scenery command
       case CHECK_SCENERY_COMMAND.name.toLowerCase(): {
         let icao = interaction.data.options[0].value.toUpperCase();
         const deferredResponse = new JsonResponse({
           type: InteractionResponseType.DEFERRED_CHANNEL_MESSAGE_WITH_SOURCE,
         });
-
         ctx.waitUntil((async () => {
-          const endpoint = `https://discord.com/api/webhooks/${env.DISCORD_APPLICATION_ID}/${interaction.token}`;
-          try {
-            let data = await getSceneryVersion(icao);
-            if (!data || !data.sid) {
-              await DiscordRequest(env, endpoint, {
-                method: 'POST',
-                body: JSON.stringify({
-                  content: `❌ No scenery is found with the ID ${icao}`,
-                }),
-              });
-              return;
-            }
-
-            const result = await checkReleased(data.sid);
-            if (!result) {
-              await DiscordRequest(env, endpoint, {
-                method: 'POST',
-                body: JSON.stringify({
-                  content: `❌ No scenery found or error checking for ${icao}`,
-                }),
-              });
-              return;
-            }
-
-            await DiscordRequest(env, endpoint, {
-              method: 'POST',
-              body: JSON.stringify({
-                content: `Scenery ${icao} found with the newest version: ${data.sid} (${new Date(data.date).toLocaleDateString('en-US', {year: 'numeric', month: 'long', day: '2-digit',})})` +
-                  (result.included ? ` and it's included in X-Plane ${result.latest}` : ` but NOT included in X-Plane ${result.latest}`),
-                components: [
-                  {
-                    type: MessageComponentTypes.ACTION_ROW,
-                    components: [
-                      {
-                        type: MessageComponentTypes.BUTTON,
-                        custom_id: `download_button_${data.sid}`,
-                        label: 'Download',
-                        style: ButtonStyleTypes.PRIMARY,
-                      },
-                      {
-                        type: MessageComponentTypes.BUTTON,
-                        label: 'Gateway Scenery Map',
-                        style: ButtonStyleTypes.LINK,
-                        url: `https://x-plane.cleverest.eu/#/overlay:scenery%3D${icao}%2F${data.sid}`,
-                      },
-                    ],
-                  },
-                ],
-              }),
-            });
-          } catch (error) {
-            console.error(`error ${error}`)
-            await DiscordRequest(env, endpoint, {
-                method: 'POST',
-                body: JSON.stringify({
-                  content: `Failed getting scenery of ${icao}`,
-                }),
-              });
-              return;
-          }
+          sceneryHandler(env, interaction, icao)
         })());
         return deferredResponse;
       }
+      //list-atc command
       case CHECK_ONLINE_ATC.name.toLowerCase():{
         const deferredResponse = new JsonResponse({
           type: InteractionResponseType.DEFERRED_CHANNEL_MESSAGE_WITH_SOURCE,
@@ -146,6 +78,7 @@ router.post('/', async (request, env, ctx) => {
         ctx.waitUntil(sendOnlineATC(env, interaction))
         return deferredResponse
       }
+      //notify command
       case MONITOR_VATSIM.name.toLowerCase():{
         const deferredResponse = new JsonResponse({
           type: InteractionResponseType.DEFERRED_CHANNEL_MESSAGE_WITH_SOURCE,
@@ -153,6 +86,7 @@ router.post('/', async (request, env, ctx) => {
         ctx.waitUntil(addReminder(interaction.data.options[0].value, interaction, env))
         return deferredResponse
       }
+      //remove command
       case REMOVE_NOTIF.name.toLowerCase():{
         const deferredResponse = new JsonResponse({
           type: InteractionResponseType.DEFERRED_CHANNEL_MESSAGE_WITH_SOURCE,
@@ -160,13 +94,18 @@ router.post('/', async (request, env, ctx) => {
         ctx.waitUntil(removeNotification(interaction.data.options[0].value, interaction, env))
         return deferredResponse
       }
+      //Handle unknown command
       default:
         return new JsonResponse({ error: 'Unknown Type' }, { status: 400 });
     }
   }
+
+  //Handle component interactions
   if (interaction.type === InteractionType.MESSAGE_COMPONENT) {
+    //Check the component id
     const componentId = interaction.data.custom_id;
 
+    //Download button component
     if (componentId.startsWith('download_button_')) {
       const sceneryId = componentId.replace('download_button_', '');
       // Delete message with token in request body
@@ -180,6 +119,8 @@ router.post('/', async (request, env, ctx) => {
         console.error('Error sending message:', err);
       }
     }
+
+    //switch atc type button component
     else if (componentId.startsWith('atc_type_')){
       const type = componentId.replace('atc_type_', '');
       try{
@@ -192,6 +133,8 @@ router.post('/', async (request, env, ctx) => {
         console.error('Error sending message:', err);
       }
     }
+
+    //Handle unknown component
     return new JsonResponse({
       type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
       data: {
@@ -199,25 +142,13 @@ router.post('/', async (request, env, ctx) => {
       },
     });
   }
+
+  //Handle unknown interaction type
   console.error('Unknown Type');
   return new JsonResponse({ error: 'Unknown Type' }, { status: 400 });
 });
+//Handle unknown route
 router.all('*', () => new Response('Not Found.', { status: 404 }));
-
-async function verifyDiscordRequest(request, env) {
-  const signature = request.headers.get('x-signature-ed25519');
-  const timestamp = request.headers.get('x-signature-timestamp');
-  const body = await request.text();
-  const isValidRequest =
-    signature &&
-    timestamp &&
-    (await verifyKey(body, signature, timestamp, env.DISCORD_PUBLIC_KEY));
-  if (!isValidRequest) {
-    return { isValid: false };
-  }
-
-  return { interaction: JSON.parse(body), isValid: true };
-}
 
 export default {
   //Get discord request
