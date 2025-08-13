672cca625fafaeb36ff8cddc4d143e9d82af52aa
Realxeng <realxengpandelaki@gmail.com>
2025-08-13 14:43:24 +0800

    fix missing type parameter

--- src/model/atc.js
+++ src/model/atc.js
@@ -1,6 +1,6 @@
 import { sendOnlineATCMessage } from "../view/discordMessages"
 
-export async function sendOnlineATC(env, covSort, webhookEndpoint){
+export async function sendOnlineATC(env, covSort, webhookEndpoint, type){
     let response = []
     const highestCoverage = Object.keys(covSort)[0]
     let field = []
