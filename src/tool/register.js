5f38417bf4cc610b9f8b48344adcaaae49774331
Realxeng <realxengpandelaki@gmail.com>
2025-09-10 15:40:38 +0800

    remove get route from commands

--- src/tool/register.js
+++ src/tool/register.js
@@ -1,4 +1,4 @@
-import { TEST_COMMAND, CHECK_SCENERY_COMMAND, CHECK_ONLINE_ATC, MONITOR_VATSIM, REMOVE_NOTIF, GET_ROUTE } from './commands.js';
+import { TEST_COMMAND, CHECK_SCENERY_COMMAND, CHECK_ONLINE_ATC, MONITOR_VATSIM, REMOVE_NOTIF } from './commands.js';
 import dotenv from 'dotenv';
 import process from 'node:process';
 
