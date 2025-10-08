3433f37780ead2b069369d6e3148104fe625d969
Realxeng <realxengpandelaki@gmail.com>
2025-10-08 22:21:48 +0800

    removed unnecessary folder

--- src/tool/base64.js
+++
@@ -1,9 +0,0 @@
-export function base64ToBlob(base64, contentType = 'application/octet-stream') {
-    const binaryString = atob(base64);
-    const len = binaryString.length;
-    const bytes = new Uint8Array(len);
-    for (let i = 0; i < len; i++) {
-        bytes[i] = binaryString.charCodeAt(i);
-    }
-    return new Blob([bytes], { type: contentType });
-}
